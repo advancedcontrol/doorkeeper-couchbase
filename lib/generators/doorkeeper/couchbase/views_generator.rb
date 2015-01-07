4bed895d410d65bec95478e0535fcba991ebd745
Stephen von Takach <steve@advancedcontrol.com.au>
2015-01-07 17:24:24 +1100

    fix generator

--- lib/generators/doorkeeper/couchbase/views_generator.rb
+++ lib/generators/doorkeeper/couchbase/views_generator.rb
@@ -6,7 +6,11 @@ module Doorkeeper
 
       def install
         path = File.expand_path('../../../../doorkeeper/orm/couchbase', __FILE__)
+        
         puts "installing design documents at #{path}"
+        require "#{path}.rb"
+        ::Doorkeeper::Orm::Couchbase.initialize_models!
+        
         ::Couchbase::Model::Configuration.design_documents_paths = [path]
         ::Doorkeeper::AccessToken.ensure_design_document!
         ::Doorkeeper::AccessGrant.ensure_design_document!
